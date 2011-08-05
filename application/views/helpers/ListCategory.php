bd41eb0d3aafae6c123a738ec09b0c984f5bafb8
morontt <morontt@gmail.com>
2011-08-05 09:20:16 +0000

    Правильная навигация по категориям и подкатегориям.
    git-svn-id: http://zadachnik-zend-blog.googlecode.com/svn/trunk@63 52091e75-ee3c-fc6c-1622-873284527cad

--- application/views/helpers/ListCategory.php
+++ application/views/helpers/ListCategory.php
@@ -2,9 +2,35 @@
 
 class Zend_View_Helper_ListCategory extends Zend_View_Helper_Abstract
 {
+    protected $_arrayCategory;
+    protected $_child;
+
+    protected function subCategory($arrayChild, $i)
+    {
+        $subResult = '';
+
+        foreach ($arrayChild as $value) {
+            foreach ($this->_arrayCategory as $value2) {
+                if ($value2['id'] == $value) {
+                    $subLink = $value2['link'];
+                    $subResult .= '<li style="padding-left: ' . $i*10 . 'px;">' . $subLink . '</li>' . PHP_EOL;
+                    if (isset($this->_child[$value2['id']])) {
+                        $subResult .= $this->subCategory($this->_child[$value2['id']], $i + 1);
+                    }
+                }
+            }
+        }
+
+        return $subResult;
+    }
+
     public function listCategory()
     {
-	    $arrayLinks = array();
+	    $tree = new Application_Model_TreeCategory;
+        $child = $tree->child;
+        $this->_child = $child;
+        
+        $arrayLinks = array();
 
         foreach ($this->view->nameCategory as $key => $value) {
             $link = '<a href="' . $this->view->url(array('id' => $key), 'category')
@@ -15,12 +41,18 @@ class Zend_View_Helper_ListCategory extends Zend_View_Helper_Abstract
                                   'link' => $link);
         }
 
-        //Zend_Debug::dump($arrayLinks); die;
+        $this->_arrayCategory = $arrayLinks;
+        //Zend_Debug::dump($this->_child); die;
 
         $result = '<ul class="My_Navigation">' . PHP_EOL;
 
         foreach ($arrayLinks as $value) {
-            $result .= '<li>' . $value['link'] . '</li>' . PHP_EOL;
+            if ($value['parent_id'] == NULL) {
+                $result .= '<li>' . $value['link'] . '</li>' . PHP_EOL;
+                if (isset($child[$value['id']])) {
+                    $result .= $this->subCategory($child[$value['id']], 1);
+                }
+            }
         }
 
         $result .= '</ul>' . PHP_EOL;
